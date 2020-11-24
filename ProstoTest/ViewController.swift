e15ad08399cddd2005bf4db8513a5324c0cbacff
vladimir.volodin <vladimir.volodin@unitbean.io>
2020-11-24 18:40:32 +0400

    Initial Commit

---
+++ ProstoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProstoTest
+//
+//  Created by Vladimir on 24.11.2020.
+//  Copyright © 2020 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
